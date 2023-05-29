a84698ea303f4f29140417d16505d27ca31d80cc
Sheraz <sherazarshad419@gmail.com>
2023-05-29 23:00:39 +0500

    hello from api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import packages
 const express = require("express");
 const routes = require("./src/router");
+const mongoose = require("mongoose");
 
 // Middlewares
 const app = express();
@@ -11,4 +12,13 @@ app.use("/", routes);
 
 // connection
 const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Server Started ${port}`));
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb+srv://sheraz:jutt5000@bot-api.mrqmd6w.mongodb.net/Bot-API?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(port, () => console.log(`Server Started ${port}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+router.get("/", (req, res) => {
+  res.send("Hello From API");
+});
+
 // get all users
 router.get("/users", userController.users);
 // create user
